b4b177bf2f99b7137455879c410534729515412f
Jawad Ismail <jawad.ismail@10pearls.com>
2023-04-18 18:06:15 +0500

    share and unseal done

--- app/api/store/route.ts
+++ app/api/store/route.ts
@@ -1,6 +1,7 @@
 import { NextRequest, NextResponse } from "next/server";
-import redis from "@/src/lib/redis";
 import { generateId } from "@/src/services/encryption";
+import { REDIS_KEY } from "@/src/lib/constants";
+import redis from "@/src/lib/redis";
 
 export const config = {
   runtime: "edge",
@@ -17,20 +18,19 @@ export async function POST(req: NextRequest) {
   const { encrypted, ttl, reads, iv } = (await req.json()) as Request;
   
   const id = generateId();
-  // const key = ["envshare", id].join(":");
+  const key = [REDIS_KEY, id].join(":");
 
-  // const tx = redis.multi();
-  // tx.hset(key, {
-  //   remainingReads: reads > 0 ? reads : null,
-  //   encrypted,
-  //   iv,
-  // });
-  // if (ttl) {
-  //   tx.expire(key, ttl);
-  // }
-  // tx.incr("envshare:metrics:writes");
+  const tx = redis.multi();
+  tx.hset(key, {
+    remainingReads: reads > 0 ? reads : null,
+    encrypted,
+    iv,
+  });
+  if (ttl) {
+    tx.expire(key, ttl);
+  }
+  tx.incr(`${REDIS_KEY}:metrics:writes`);
 
-  // await tx.exec();
-
+  await tx.exec();
   return NextResponse.json({ id });
 }
--- src/services/encode.ts
+++ src/services/encode.ts
@@ -1,3 +1,4 @@
+import { ENCRYPTION_KEY_LENGTH, ID_LENGTH } from "../lib/constants";
 import { fromBase58, toBase58 } from "../lib/utils";
 
 export function encodeCompositeKey(version: number, id: string, encryptionKey: Uint8Array): string {
@@ -7,4 +8,19 @@ export function encodeCompositeKey(version: number, id: string, encryptionKey: U
   const compositeKey = new Uint8Array([version, ...fromBase58(id), ...encryptionKey]);
 
   return toBase58(compositeKey);
+}
+
+export function decodeCompositeKey(compositeKey: string): { id: string; encryptionKey: Uint8Array; version: number } {
+  const decoded = fromBase58(compositeKey);
+  const version = decoded.at(0);
+
+  if (version === 1 || version === 2) {
+    return {
+      id: toBase58(decoded.slice(1, 1 + ID_LENGTH)),
+      encryptionKey: decoded.slice(1 + ID_LENGTH, 1 + ID_LENGTH + ENCRYPTION_KEY_LENGTH),
+      version,
+    };
+  }
+
+  throw new Error(`Unsupported composite key version: ${version}`);
 }
